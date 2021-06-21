feffaed0d32f7192ca74256b8facbfd3bf7ca7a0
Elaine Alasagas <ealasagas@gmail.com>
2021-06-21 14:33:46 -0700

    angular practice

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵisObservable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../github-followers.service';
 import { DataService } from '../services/data.services';
+import { Observable } from 'rxjs';
+import { combineLatest, pipe  } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'github-followers',
@@ -11,10 +15,51 @@ export class GithubFollowersComponent implements OnInit {
 
   followers: any;
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService
+    ) { }
 
   ngOnInit() {
-    this.service.getAll()
+    // let obs = combineLatest([
+    //     this.route.paramMap,
+    //     this.route.queryParamMap
+    //   ])
+    //   .switchMap((combined: { get: (arg0: string) => any; }[]) => {
+    //       let id = combined[0].get('id');
+    //       let page = combined[1].get('page');
+  
+    //       // this.service.getAll({ id: id, page: page})
+    //       this.service.getAll();
+    //     })
+   
+      
+    
+    // obs.subscribe((followers: any) => {this.followers = followers}); 
+   
+    
+    
+  let obs = combineLatest([
+        this.route.paramMap,
+        this.route.queryParamMap
+      ])
+ 
+    obs.subscribe(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+
+      // this.service.getAll({ id: id, page: page})
+      this.service.getAll()
       .subscribe(followers => this.followers = followers)
+    })
+
+
+    //CAN ALSO USE SNAPSHOT
+    // this.route.paramMap
+    //   .subscribe(params => {});
+    // this.route.queryParamMap
+    //   .subscribe(params => {});
+    // let id = this.route.snapshot.paramMap.get('id')
+    // let page = this.route.snapshot.queryParamMap.get('page');
   }
 }
--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: {page: 1, order: 'newest'}
+    });
   }
+  ngOnInit() {
+    //for navigating away
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
+    // this.route.paramMap
+    //   .subscribe(params => {
+    //     let id = +params.get('id')!;  //use +params to have variable as a number instead of string
+    //     console.log(id);
+    //   })
+
+      // console.log("GitHubProfile Oninit");
+  }
+
 
 }
